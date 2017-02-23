afb0d51c45b4beff0ba7c7d2dd3e4cd03a6216f0
frednovack <lucas.fonsecar@gmail.com>
2017-02-22 22:10:42 -0300

    inicio de projeto

---
+++ teste_ios/teste_ios/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  teste_ios
+//
+//  Created by Frederico Novack Amaral Pereira on 2/22/17.
+//  Copyright © 2017 frednovack. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
