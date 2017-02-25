e0f8703656bcd9b5af7918b017c0dbd6b554b867
frednovack <fredynovack@gmail.com>
2017-02-25 19:04:56 -0300

    Preparando Horizontal Menu

---
+++ teste_ios/teste_ios/GlobalTabViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GlobalTabViewController.swift
+//  teste_ios
+//
+//  Created by Frederico Novack Amaral Pereira on 2/25/17.
+//  Copyright © 2017 frednovack. All rights reserved.
+//
+
+import UIKit
+
+class GlobalTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
